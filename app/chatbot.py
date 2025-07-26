91a6449056051bb416afbe6137cc04213ad0e7c5
Nora Nur Fadila <153160223+noranur123@users.noreply.github.com>
2025-07-26 14:43:59 +0700

    Update chatbot.py

--- app/chatbot.py
+++ app/chatbot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from .models import ChatRating, db, ChatMessage
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 chatbot = Blueprint('chatbot', __name__)
 
